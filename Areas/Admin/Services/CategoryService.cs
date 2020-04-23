9de536511e09a775fd98615cd63b367c1bfed6ab
Achref Bouadila <a_bouadila@live.fr>
2020-04-24 00:02:32 +0100

    create GetCategories API

---
+++ Areas/Admin/Services/CategoryService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantAspCore3.Data;
+using RestaurantAspCore3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAspCore3.Areas.Admin.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryService(ApplicationDbContext _db)
+        {
+            this._db = _db;
+        }
+
+        public async Task<IEnumerable<Category>> GetCategories()
+        {
+            return await _db.categories.ToListAsync();
+        }
+    }
+}
